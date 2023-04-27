98f64c6ec19b8fa4e3c67a2b0bf865e7234bb8fd
kenedy lima <kenedytorreslima@gmail.com>
2023-04-27 12:44:29 -0300

    allowing cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +10,8 @@ app.use(
     extended: false,
   })
 );
+
+app.use(cors());
 
 const baseUrl = "";
 
